5a9f93fad922f0ff6f4e0a02e9a58c1321018bed
liuhuan-hz20085476 <hz20085476@huize.com>
2022-11-10 20:21:46 +0800

    init

---
+++ practice/redux/index.ts
@@ -0,0 +1,31 @@
+class Store {
+    constructor(reducer, initialState) {
+        
+    }
+
+    getState() {
+
+    }
+
+    dispatch () {
+
+    }
+
+    substribe() {
+
+    }
+}
+
+
+
+export const creteStore = (reducer, initialState) => {
+
+}
+
+export const combineReducer = () => {
+
+}
+
+export const applyMiddleware = () => {
+    
+}
