fa45faaed3968df7d06b948a0fd4e5aeaf23acf8
StoneCode13 <hellostone13@163.com>
2023-07-02 16:53:34 +0800

    wip

---
+++ practice/algorithm/day03.ts
@@ -0,0 +1,37 @@
+// 203.移除链表元素
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    
+}
+
+ function removeElements(head: ListNode | null, val: number): ListNode | null {
+
+};
+
+
+
+
+
+/**
+ * 自己看到题目的第一想法
+ *     符合条件的链表该如何表示，第一个节点没有考虑到，应该加一个虚拟的节点
+● 看完代码随想录之后的想法 
+    
+● 自己实现过程中遇到哪些困难 
+     
+● 今日收获，记录一下自己的学习时长
+    1.5小时 
+ */
